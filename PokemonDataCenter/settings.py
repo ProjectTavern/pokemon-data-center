d137b223a37814ec71210af586964bf2f83148fb
sieeid <sieeid8128@gmail.com>
2019-02-18 23:13:45 +0900

    생성

--- PokemonDataCenter/settings.py
+++ PokemonDataCenter/settings.py
@@ -25,11 +25,13 @@ SECRET_KEY = '$i1(b$p(vfsy#^49emr*4&x+gsqcbp4by_16!7jgj@pp#m$(@&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
-
+CORS_ORIGIN_ALLOW_ALL = True
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pokemon',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'PokemonDataCenter.urls'
